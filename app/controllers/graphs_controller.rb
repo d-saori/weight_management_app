fa8989f758ebbde66df39761707d9b2b2e34e2fa
d-saori <60836449+d-saori@users.noreply.github.com>
2020-06-12 23:38:34 +0900

    DBに保存できるように追加

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -4,12 +4,29 @@ class GraphsController < ApplicationController
     # gon.chart_label = ['1/1', '1/2', '1/4', '1/5', '1/6', '1/7']
     # # null を nil にしないといけないので注意！
     # gon.chart_data = [60.3, 61.1, 60.8, nil, 60.5, 61.4]
+    # 記録済みの日付データ
+    gon.recorded_dates = current_user.graphs.map(&:date)
   end
 
   def create
+    @graph = current_user.graphs.build(graph_params)
+    date = @graph.date.strftime('%Y/%-m/%-d')
+    if @graph.save
+      flash[:notice] = "#{date}の記録を追加しました"
+    else
+      flash[:alert] = 'エラーが発生しました'
+    end
+    redirect_to root_path
   end
 
   def update
+
+  end
+
+  private
+
+  def graph_params
+    params.require(:graph).permit(:date, :weight)
   end
   
 end
